d479b0192ae215b50f050d4eb26006b890233371
Espresso521 <kotaku20220424@gmail.com>
2024-02-20 15:05:28 +0900

    1. Add login bear animation : )

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/LoginActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/LoginActivity.kt
@@ -2,6 +2,8 @@ package jp.co.yumemi.android.code_check
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -9,13 +11,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import app.rive.runtime.kotlin.RiveAnimationView
+import app.rive.runtime.kotlin.core.ExperimentalAssetLoader
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.api.HttpRequestState
 import jp.co.yumemi.android.code_check.databinding.ActivityLoginBinding
 import jp.co.yumemi.android.code_check.viewmodel.LoginViewModel
+import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.launch
-import java.io.IOException
-import kotlin.math.log
 
 @AndroidEntryPoint
 open class LoginActivity : AppCompatActivity(R.layout.activity_login) {
@@ -26,7 +29,9 @@ open class LoginActivity : AppCompatActivity(R.layout.activity_login) {
     private lateinit var binding: ActivityLoginBinding
 
     private val loginViewModel: LoginViewModel by viewModels()
+    private lateinit var loginAV: RiveAnimationView
 
+    @OptIn(ExperimentalAssetLoader::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -46,9 +51,11 @@ open class LoginActivity : AppCompatActivity(R.layout.activity_login) {
                         }
                         is HttpRequestState.Success -> {
                             Log.d(TAG, "app login Success value is ${it.value}")
-                            onLoginSuccess()
+                            loginAV.fireState("LoginStateMachine", "success")
+                            loginAV.postDelayed(Runnable { onLoginSuccess() }, 2000)
                         }
                         is HttpRequestState.Error -> {
+                            loginAV.fireState("LoginStateMachine", "fail")
                             Toast.makeText(
                                 this@LoginActivity,
                                 "Username or password Error",
@@ -60,6 +67,52 @@ open class LoginActivity : AppCompatActivity(R.layout.activity_login) {
                 }
             }
         }
+
+        loginAV = binding.loginAnimationView
+        binding.loginButton.setOnClickListener {
+            loginAV.setBooleanState("LoginStateMachine", "hands_up", false)
+            loginAV.postDelayed(Runnable { loginViewModel.onLogin() }, 2000)
+        }
+
+        binding.nameEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let {
+                    if(s.isNotEmpty()) {
+                        loginAV.setBooleanState("LoginStateMachine", "Check", true)
+                    } else {
+                        loginAV.setBooleanState("LoginStateMachine", "Check", false)
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // do nothing
+            }
+        })
+
+        binding.passwordEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let {
+                    if(s.isNotEmpty()) {
+                        loginAV.setBooleanState("LoginStateMachine", "hands_up", true)
+                    } else {
+                        loginAV.setBooleanState("LoginStateMachine", "hands_up", false)
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // do nothing
+            }
+        })
     }
 
     protected open fun onLoginSuccess() {
